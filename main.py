9f9b82df7785d41ae4393a1c94bb0005b0ab0c54
SweetCoder <89735308+SkotnikovViktor@users.noreply.github.com>
2022-06-30 19:19:33 +0300

    Рабочий бот

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from token import * 
+import telebot
+bot = telebot.TeleBot("5505530308:AAGoiiUP5dD6GP6eM_3b5AfHJQrVdDXFXQI")
+
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.chat.id,'<b>Привет!</b>',parse_mode = 'html')
+
+bot.polling(none_stop = True)
