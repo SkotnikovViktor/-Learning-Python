85a0c1a281ceead6c833bf3eb5a113135857b536
SweetCoder <89735308+SkotnikovViktor@users.noreply.github.com>
2022-07-01 21:36:19 +0300

    Добавил комментарии
    Добавление комментариев в коде

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+# Импорт библиотеки telebot 
 import telebot
+# Создание переменной с токеном
 bot = telebot.TeleBot("5505530308:AAGoiiUP5dD6GP6eM_3b5AfHJQrVdDXFXQI")
 
-
+# Создание кнопки в чате + вложенная функция
 @bot.message_handler(commands = ['start'])
 def start(message):
     bot.send_message(message.chat.id,'<b>Привет!</b>',parse_mode = 'html')
 
+# Запуск бота
 bot.polling(none_stop = True)
