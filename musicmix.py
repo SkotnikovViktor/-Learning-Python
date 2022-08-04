5b5c2c6e1bd10cb15b084f818a0bd4f284538dc6
Viktor Skotnikov <Skotnikov2007@gmail.com>
2022-08-04 17:44:30 +0300

    New

---
+++ musicmix.py
@@ -0,0 +1,15 @@
+import random
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+def music():
+    c = ['Aria.mp3', 'basta.mp3']
+    a = random.choice(c)
+    pygame.mixer.music.load(a)
+    pygame.mixer.music.play(0)
+    while True:
+        pygame.time.delay(100)
+
+#привет Виктор)
+music()
