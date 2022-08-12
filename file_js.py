ca1d408625665039c092458f92dcaad651e8c60c
SweetCoder <89735308+SkotnikovViktor@users.noreply.github.com>
2022-08-12 17:24:28 +0300

    Новый вовод json (через библеотеку json)

--- file_js.py
+++ file_js.py
@@ -1,6 +1,6 @@
 import json
-from pprint import pprint
 with open('file.json','r') as file:
     save_file = json.load(file)
-pprint(save_file)
+print(json.dumps(save_file,sort_keys = True, indent = 4))
+
 
