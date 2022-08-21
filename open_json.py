9b8c6f19098a056a2835fb854fd6aff5d2e2887b
Viktor <Skotnikov2007@gmail.com>
2022-08-21 17:30:16 +0300

    Добавлен код\ добавлены строки подсказки(комментарии)

--- open_json.py
+++ open_json.py
@@ -4,3 +4,6 @@ import json
 with open('file.json', 'r') as f:
     # Сохранение в переменную
     json_data = json.load(f)
+# Первый пробный код (не работает)
+for i,y in json_data:
+    print(i[y])
