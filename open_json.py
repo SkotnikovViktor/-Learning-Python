105289d767b090a0753d78295822a8178baed2a7
Viktor <Skotnikov2007@gmail.com>
2022-08-22 15:35:33 +0300

    Изменен файл file.json

--- open_json.py
+++ open_json.py
@@ -5,5 +5,4 @@ with open('file.json', 'r') as f:
     # Сохранение в переменную
     json_data = json.load(f)
 # Первый пробный код (не работает)
-for i,y in json_data:
-    print(i[y])
+print(json_data)
