a7f324d38a26b70b033e25baf033ccff91d52f02
Viktor <Skotnikov2007@gmail.com>
2022-08-23 14:10:10 +0300

    Добавлен код по красивому выводу json без библиотеки json

---
+++ .test_open_json.py
@@ -0,0 +1,2 @@
+two = ['True','55',55,'Up']
+print(two[0])
--- open_json.py
+++ open_json.py
@@ -1,8 +1,8 @@
 # Импорт библиотеки json
 import json
 # Открытие файла
-with open('file.json', 'r') as f:
+with open('file.json', 'r') as file:
     # Сохранение в переменную
-    json_data = json.load(f)
+    json_data = json.load(file)
 for one,two in json_data.items():
     print(one,':',two)
