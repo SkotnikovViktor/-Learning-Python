a3eb5f100d7574d29109086adce05097e0638cf9
Viktor <Skotnikov2007@gmail.com>
2022-08-23 14:34:21 +0300

    Поправлена запятая)

--- open_json.py
+++ open_json.py
@@ -1,8 +1,10 @@
 # Импорт библиотеки json
 import json
-# Открытие файла
+# Открытие файла для запись в переменную
 with open('file.json', 'r') as file:
-    # Сохранение в переменную
+    # Сохранение файла в переменную
     json_data = json.load(file)
+    # Цикл for
 for one,two in json_data.items():
-    print(one,':',two)
+    print(one,':',two,',')
+
